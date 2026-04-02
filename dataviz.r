853dac708ccab00f5d1d033da27fa41331729258
gnix22 <gjnix22@gmail.com>
2026-04-02 19:54:48 -0400

    added r file for EDA

---
+++ dataviz.r
@@ -0,0 +1,2 @@
+library(ggplot2,av)
+raw_pcm <- read_audio_bin("/home/gnix/Music/Mort Garson - Mother Earth's Plantasia/Mort Garson - Mother Earth's Plantasia - 01-05 Concerto for Philodendron & Pothos.flac")
