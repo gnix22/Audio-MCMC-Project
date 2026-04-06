56dd5da33c93077509ff9d8fd7ad9a39dea6c683
gnix22 <gjnix22@gmail.com>
2026-04-06 00:12:14 -0400

    need to figure out plot still

--- dataviz.r
+++ dataviz.r
@@ -2,4 +2,4 @@ library(av)
 # load and normalize pcm data to match that of python file
 df <- read.csv("data/divinity_audio_sample_data.csv")
 
-plot(df$Time, df$Amplitude)
+plot(df$Time, df$Amplitude, type = "l")
